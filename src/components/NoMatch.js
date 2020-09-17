4bec3afea2b71f6254ea21ab2f2f2fa469661372
vidyasujaya <sujaya.vidya@gmail.com>
2020-09-17 01:03:10 -0400

    configure routers for the pages home, contact, about and resources, and add contents to each page

---
+++ src/components/NoMatch.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export const NoMatch = () => {
+  return (
+    <div>
+      <h4>No Match</h4>
+    </div>
+  );
+};
---
+++ src/components/Resources.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export const Resources = () => {
+  return (
+    <div>
+      <h1>Resources/More Information</h1>
+      <p>
+        To access official government/SQI documents regarding their planning
+        process,{" "}
+        <a href="https://ccmp-mpcc.us13.list-manage.com/track/click?u=36c1b55af25203f26288d151a&id=dc442278f3&e=617850c6c1">
+          click here
+        </a>
+        , and to see visuals of McGill's plans for the site and other documents,
+        see{" "}
+        <a href="https://ccmp-mpcc.us13.list-manage.com/track/click?u=36c1b55af25203f26288d151a&id=e2b0cc6ff1&e=617850c6c1">
+          this link
+        </a>
+        .
+      </p>
+      <p>
+        For more information on our coalition's documents and communications,{" "}
+        <a href="https://ccmp-mpcc.us13.list-manage.com/track/click?u=36c1b55af25203f26288d151a&id=bc99531bd9&e=617850c6c1">
+          see this link
+        </a>
+        .
+      </p>
+    </div>
+  );
+};
